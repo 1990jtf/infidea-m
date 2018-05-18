1d31619f85a545dc0f5ecdff11b367ae53ee1ff7
Tengfei Jiang <1990jtf@gmail.com>
2018-05-18 12:28:05 +0800

    v1.0.3 add fastclick fix

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import App from './App'
 import router from './router'
 import './common/stylus/reset.styl'
 import './common/stylus/base.styl'
+import fastclick from 'fastclick'
 import 'animate.css'
+
+// 解决移动设备下的click事件 300ms的延迟
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
